f4909698465ad85b5bfce06a6ad7982c78fd9447
Fabio Ferreira <fabiomataferreira@gmail.com>
2017-10-22 17:59:19 -0400

    routes started working

---
+++ gifs/src/app/listaCategorias/listaCategorias.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id, 
+    selector: 'lista-categoria',
+    templateUrl: './listaCategorias.component.html'
+})
+
+export class ListaCategoriaComponent {}
